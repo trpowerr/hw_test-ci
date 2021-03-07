2c53319d1ace23db10a12d73749b26cc7192665f
Tryakin Mikhail <mixazbubl@yandex.ru>
2021-03-07 14:15:40 +0300

    fix

--- src/js/arr.js
+++
@@ -1,7 +0,0 @@
-const personArr = [
-  { name: 'мечник', health: 10 },
-  { name: 'маг', health: 100 },
-  { name: 'лучник', health: 80 },
-];
-
-export default personArr.sort((a, b) => b.health - a.health);
